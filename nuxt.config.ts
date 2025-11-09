c16d53d0b97d4417497c09ef0664c6294b6423ff
ProudGuy69 <idioticbatteringram@gmail.com>
2025-11-09 18:06:47 -0500

    finishing touches

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  modules: ['@nuxt/ui', '@nuxtjs/robots', '@nuxtjs/sitemap'],
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'NH Discord / New Hampshire Dating & Local Community',
+      meta: [
+        { name: 'description', content: 'Join NH Discord a local New Hampshire dating and social community where locals meet naturally. Free and real connections.' },
+        { name: 'keywords', content: 'NH Discord, New Hampshire Dating, Live Free and Love, NH Community, Local Dating Discord' },
+        { property: 'og:title', content: 'NH Discord Server' },
+        { property: 'og:description', content: 'Safe space and communities for the likes of all people to join and interact with' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://nhdiscord.com' },
+        { property: 'og:image', content: '' }
+      ],
+    }
+  },
+  site: {
+    url: "https://nhdiscord.com",
+    name: "New Hampshire Discord"
+  }
 })
