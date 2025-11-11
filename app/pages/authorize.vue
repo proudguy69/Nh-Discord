40a226379635b7fa445c21003ea80e0c48f01426
Oliver <jokejupa@gmail.com>
2025-11-11 11:19:46 -0500

    finish user auth

--- app/pages/authorize.vue
+++ app/pages/authorize.vue
@@ -1,7 +1,32 @@
 <template>
     if you see thus page please wait while you are being redirected
+    <br>
+    data = {{ data }}
 </template>
 
 <script setup>
+
+const route = useRoute()
+const router = useRouter()
+
+const user_info = inject("user_info")
+
+let data = ref({})
+
+onMounted(async () => {
+    const code = route.query.code
+    if (!code) {
+        router.push('/')
+    } else {
+        const response = await fetch(`https://api.nhdiscord.com/authorize?code=${code}`)
+        data.value = await response.json()
+        
+        console.log(user_info)
+        user_info.value = data.value.data
+        router.push('/')
+    }
+})
+
+
 
 </script>
