f9633d34c07a8f9f3676308704539a753d47cfe3
Oliver <jokejupa@gmail.com>
2025-11-11 11:19:46 -0500

    finish user auth

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,8 @@
 
       <template #right>
         <UNavigationMenu :items="items"/>
+        <UButton v-if="!user_info.id" icon="logos:discord-icon" variant="subtle" color="secondary" :to="authentication_uri">Login</UButton>
+        <UButton v-if="user_info.id" variant="subtle" color="secondary" :avatar="{src: `https://cdn.discordapp.com/avatars/${user_info.id}/${user_info.avatar}.png`}" @click="logout">Account</UButton>
         <UColorModeButton />
       </template>
       
@@ -15,13 +17,24 @@
 
 <script setup>
 
-const authentication_uri = "https://discord.com/oauth2/authorize?client_id=1304965391507914782&response_type=code&redirect_uri=http%3A%2F%2Fapi.nhdiscord.com%2Fauthorize&scope=identify+email+guilds.join"
+const uris = {
+  dev: "https://discord.com/oauth2/authorize?client_id=1304965391507914782&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauthorize&scope=identify+guilds.join+email",
+  prod: "https://discord.com/oauth2/authorize?client_id=1304965391507914782&response_type=code&redirect_uri=https%3A%2F%2Fnhdiscord.com%2Fauthorize&scope=identify+guilds.join+email"
+}
+
+const authentication_uri = uris.dev
+const user_info = ref({})
+
+provide("user_info", user_info)
+
+function logout() {
+  user_info.value = {}
+}
 
 const items = [
   {
-    label: "Login",
-    icon: "logos:discord-icon",
-    to: authentication_uri
+    label: "about",
+    to: '/about'
   }
 ]
 
