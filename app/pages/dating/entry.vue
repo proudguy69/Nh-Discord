0d827a82505397d9d56fd403b6ef68cc7d0f4808
ProudGuy69 <idioticbatteringram@gmail.com>
2025-11-14 22:03:42 -0500

    work on dating entry page

--- app/pages/dating/entry.vue
+++ app/pages/dating/entry.vue
@@ -1,22 +1,49 @@
 <template>
-    <UPageHero v-if="!user_info.id" title="Welcome to Live free or love!" description="Please log in with discord to continue">
-        <UButton icon="logos:discord-icon" variant="subtle" color="secondary" :to="authentication_uri">Login</UButton>
-    </UPageHero>
-    <UPageHero
-    v-if="user_info.id"
-    :title="title"
-    :description="description"
-    orientation="horizontal"
-    headline="Experimental"
-    >
-        <video src="~/assets/videos/sample_video.mp4" controls>
-            <source src="~/assets/videos/sample_video.mp4" type="video/mp4">
-        </video>
+    <!--If the user is NOT logged in-->
+    <UBanner
+    title="This page is still under development, please check back again later"
+    icon="i-lucide-info"
+    />
 
-        <h2>
-            this is still being worked on, join the main server and contact paul for updates
-        </h2>
-    </UPageHero>
+    <UPage v-if="!user_info.id">
+        <UPageHero
+        title="Welcome to Live free or love!"
+        description="Please log in with discord to continue"
+        :links="links"
+        />
+
+    </UPage>
+
+
+    <UPage v-if="user_info.id">
+        
+        <UPageHero
+        :title="title"
+        :description="description"
+        orientation="horizontal"
+        headline="Experimental"
+        >
+            <video src="~/assets/videos/sample_video.mp4" controls>
+                <source src="~/assets/videos/sample_video.mp4" type="video/mp4">
+            </video>
+
+            
+        </UPageHero>
+
+        <UPageSection
+        title="Entry Form"
+        description="Fill out the form below to enter the server"
+        headline="Start here"
+        >
+            <UEmpty
+            title="Still under development"
+            description="This section is still under development, please be paitent while we work on this"
+            icon="i-lucide-file"
+            variant="subtle"
+            />
+        </UPageSection>
+    </UPage>
+    
     
 </template>
 
@@ -27,5 +54,16 @@ const description = "Join early, help shape it, and be part of something that ac
 
 const authentication_uri = inject('authentication_uri')
 const user_info = inject("user_info")
+
+const links = ref([
+    {
+        label: "Login with discord",
+        icon: "logos:discord-icon",
+        variant: "subtle",
+        color: "secondary",
+        to: authentication_uri
+
+    }
+])
 
 </script>
